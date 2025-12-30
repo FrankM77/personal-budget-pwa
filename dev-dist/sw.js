083c295ac88757f47bfd5a2fd181bc8e0d2694a9
Frank Marchese <frankmarchese01@gmail.com>
2025-12-30 18:18:58 -0500

    Update service worker configuration

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "0fd255074d07fd2dd10d374c0c11631d"
   }, {
     "url": "index.html",
-    "revision": "0.8duajb22m7c"
+    "revision": "0.ev6a4c2832o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
